3d5e7d23b6fb07685f03305c6520c40b74165235
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-07 11:09:21 +0100

    feat(entity): create tracking data entity

---
+++ src/main/kotlin/entity/TrackingData.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity
+
+import java.sql.Timestamp
+
+/** The data of the health professionals tracking.
+ * A tracking is composed by
+ * the [dateTime], the [roomId],
+ * the [healthProfessionalId] and the [trackingType] of the event.
+ */
+data class TrackingData(
+    val dateTime: Timestamp,
+    val roomId: RoomId,
+    val healthProfessionalId: HealthProfessionalId,
+    val trackingType: TrackingType
+)
+
+/** The type of the tracking event. */
+enum class TrackingType {
+    ENTER, EXIT
+}
+
+/** The [id] of the Health Professional. */
+data class HealthProfessionalId(val id: String)
+
+/** The [id] of the room. */
+data class RoomId(val id: String)
