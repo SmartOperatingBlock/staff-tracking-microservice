f6d9deb804f991d50d0b68320c0fa5a7467f16b5
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-07 17:22:30 +0100

    chore: create Kafka client

---
+++ src/main/kotlin/infrastructure/events/KafkaClient.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package infrastructure.events
+
+import application.service.TrackingServices
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import infrastructure.events.model.TrackingEventDto
+import infrastructure.events.model.toTrackingData
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import usecase.repository.TrackingDataRepository
+import java.time.Duration
+
+/** The Kafka Client to consume staff tracking events.
+ * @param dbManager the database manager for database operations.
+ */
+class KafkaClient(private val dbManager: TrackingDataRepository) : EventConsumer<TrackingEventDto> {
+
+    init {
+        listOf(System.getenv("BOOTSTRAP_SERVER_URL"), System.getenv("SCHEMA_REGISTRY_URL")).forEach {
+            requireNotNull(it) {
+                """
+                Invalid environment variable: $it
+                Check out the documentation here:
+                https://github.com/SmartOperatingBlock/bootstrap"""
+                    .trimIndent()
+            }
+        }
+    }
+
+    private val kafkaConsumer: KafkaConsumer<String, String> = KafkaConsumer(loadConsumerProperties())
+
+    override fun start() {
+        kafkaConsumer.subscribe(listOf(topic)).run {
+            while (true) {
+                kafkaConsumer.poll(Duration.ofMillis(pollingTime)).forEach { event ->
+                    val trackingEvent = jacksonObjectMapper().convertValue(event.value(), TrackingEventDto::class.java)
+                    if (trackingEvent != null) {
+                        consume(trackingEvent)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun consume(event: TrackingEventDto): Boolean =
+        TrackingServices.AddTrackingData(event.toTrackingData(), dbManager).execute()
+
+    companion object {
+        /** The topic on which consume tracking events. */
+        const val topic = "tracking-events"
+        /** The polling time. */
+        const val pollingTime: Long = 100
+    }
+}
