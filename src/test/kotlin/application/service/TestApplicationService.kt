d6e3ff44807488d340227b31ce704237661882c0
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-12 19:07:33 +0100

    test(application): add application service tests

---
+++ src/test/kotlin/application/service/TestApplicationService.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.service
+
+import application.controller.StaffTrackingController
+import application.service.mock.MockDbManager
+import entity.HealthProfessionalId
+import entity.RoomId
+import entity.TrackingData
+import entity.TrackingType
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.time.Instant
+
+class TestApplicationService : StringSpec({
+
+    val mockTrackingData = TrackingData(
+        Instant.now(),
+        RoomId("room#1"),
+        HealthProfessionalId("12345678"),
+        TrackingType.ENTER
+    )
+    var staffTrackingController = StaffTrackingController(MockDbManager())
+
+    beforeEach {
+        staffTrackingController = StaffTrackingController(MockDbManager())
+    }
+
+    "The application service should be able to add tracking data" {
+        staffTrackingController.addTrackingData(mockTrackingData) shouldBe true
+    }
+
+    "The application service should be able to get tracking data from a room id" {
+        staffTrackingController.addTrackingData(mockTrackingData)
+        staffTrackingController.getRoomTrackingData(mockTrackingData.roomId, null).isNotEmpty() shouldBe true
+    }
+
+    "The application service should be able to get tracking data from a health professional id" {
+        staffTrackingController.addTrackingData(mockTrackingData)
+        staffTrackingController.getHealthProfessionalTrackingData(
+            mockTrackingData.healthProfessionalId,
+            null
+        ).isNotEmpty() shouldBe true
+    }
+
+    "The application service should be able to get tracking data from a certain moment" {
+        val beforeInstant = Instant.now()
+        val trackingData = TrackingData(
+            Instant.now(),
+            RoomId("room#1"),
+            HealthProfessionalId("12345678"),
+            TrackingType.ENTER
+        )
+        staffTrackingController.addTrackingData(trackingData)
+        staffTrackingController.getLatestTrackingData(beforeInstant).isNotEmpty() shouldBe true
+        staffTrackingController.getLatestTrackingData(beforeInstant).first() shouldBe trackingData
+    }
+})
