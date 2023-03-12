ab1725e1f8a6960d9c88a0531b8a7ec4fc0c6b4b
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-03-12 21:34:39 +0100

    chore: create api dto

---
+++ src/main/kotlin/application/presenter/api/TrackingDataApiDto.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.api
+
+import entity.TrackingData
+import entity.TrackingType
+import kotlinx.serialization.Serializable
+
+/**
+ * Object to serialize the Tracking Data in API.
+ * It serializes the [dateTime], the [roomId], the [healthProfessionalId] and the [trackingType].
+ */
+@Serializable
+data class TrackingDataApiDto(
+    val dateTime: String,
+    val roomId: String,
+    val healthProfessionalId: String,
+    val trackingType: TrackingType
+)
+
+/** Extension method to convert a [TrackingData] to a [TrackingDataApiDto]. */
+fun TrackingData.toTrackingDataApiDto() =
+    TrackingDataApiDto(
+        this.dateTime.toString(),
+        this.roomId.id,
+        this.healthProfessionalId.id,
+        this.trackingType
+    )
